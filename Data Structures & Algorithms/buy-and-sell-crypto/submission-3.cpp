8ed3fa02b868786052ba408eb215786dcaee99be
Dishaa Singh <137612261+AnotherOnlineUser@users.noreply.github.com>
2026-05-28 12:55:05 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() < 2) return 0;
+        int left = 0, right = 1;
+        int maxprofit = 0;
+
+        while (right < prices.size()) {
+            if (prices[right] < prices[left]) {
+                left = right;
+            }
+            else {
+                int profit = prices[right] - prices[left];
+                maxprofit = max(maxprofit, profit);
+            }
+            right++;
+
+        }
+        return maxprofit;
+    }
+    };
